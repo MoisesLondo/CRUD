3fa306f4b23e4faa756ad22aa420441adf4fd87c
Luis <142854556+AstroLui@users.noreply.github.com>
2024-05-03 22:05:12 -0400

    feat: adding the Tr for responsive

--- src/Table.js
+++ src/Table.js
@@ -5,11 +5,13 @@ class Table
     list = [];
     table = document.createElement('table');
     container = document.createElement('div');
+    containerMiniTr = document.createElement('div');
     containerMain = document.createElement('div');
     constructor()
     {
         this.containerMain.className = "flex flex-col items-center";
-        this.container.className = "w-9/12 rounded-lg shadow hidden sm:block";
+        this.container.className = "w-9/12 rounded-lg shadow hidden md:block";
+        this.containerMiniTr.className = "w-11/12 grid grid-cols-1 gap-4 md:hidden";
         this.table.className = "w-full";
         this.table.innerHTML= 
         `
@@ -24,19 +26,21 @@ class Table
         `;
         this.container.appendChild(this.table);
         this.containerMain.appendChild(this.container);
+        this.containerMain.appendChild(this.containerMiniTr);
         document.body.appendChild(this.containerMain);
     }
 
-    Add(obj)
+    Add(obj, obj1)
     {
         this.table.childNodes.item(3).appendChild(obj);
+        this.containerMiniTr.appendChild(obj1)
     }
 }
 
 const table = new Table();
 const tr = new Tr(1, "Luis");
-table.Add(tr.getTr());
+table.Add(tr.getTr(), tr.getMiniTr());
 const tr1 = new Tr(2, "Isidoro");
-table.Add(tr1.getTr());
+table.Add(tr1.getTr(), tr1.getMiniTr());
 const tr2 = new Tr(3, "MiNombleEsBo");
-table.Add(tr2.getTr());
+table.Add(tr2.getTr(), tr2.getMiniTr());
--- src/Tr.js
+++ src/Tr.js
@@ -1,9 +1,12 @@
 export default class Tr
 {
     tr;
+    miniTr;
     constructor(id, name)
     {
         this.tr = document.createElement('tr');
+        this.miniTr = document.createElement('div');
+        this.miniTr.className = "bg-gray-200 p-4 rounded-lg shadow";
         this.tr.className = id%2 ? "bg-white" : "bg-gray-50";
         this.tr.innerHTML = 
         `
@@ -13,10 +16,29 @@ export default class Tr
             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">Contenido 4</td>
             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">Contenido 5</td>
         `;
+        this.miniTr.innerHTML = 
+        `
+            <div class="flex flex-col gap-1">
+                <div>
+                    <span class="text-md">${name}</span>
+                    <span class="text-sm text-gray-500">Contenido 2</span>
+                </div>
+                <div>
+                    <span class="text-lg">Contenido 3</span>
+                    <span class="text-md text-rose-500">Contenido 4</span>
+                </div>
+                <span class="text-2xl text-rose-500">Contenido 5</span>
+            </div>
+        `;
     }
 
     getTr()
     {
         return this.tr;
     }
+    getMiniTr()
+    {
+        return this.miniTr;
+    }
+
 }
