cadbe31ceb004b9abf78afb088bb736a935c52c9
Luis <142854556+AstroLui@users.noreply.github.com>
2024-05-03 22:57:29 -0400

    feat: adding the object Vehicle with content

---
+++ src/objects/Vehiculo.js
@@ -0,0 +1,42 @@
+export default class Vehicle
+{
+    constructor(brand, model, colour, licensePlate, yearVehicle, pictureVehicle)
+    {
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+        this.licensePlate = licensePlate;
+        this.yearVehicle = yearVehicle;
+        this.pictureVehicle = pictureVehicle;
+    }
+    
+    asigOwn(people)
+    {
+        this.own = people; 
+    }
+
+    getOwn()
+    {
+        return this.own;
+    }
+    getBrand()
+    {
+        return this.brand;
+    }
+    getColour()
+    {
+        return this.colour;
+    }
+    getLicensePlate()
+    {
+        return this.licensePlate;
+    }
+    getYearVehicle()
+    {
+        return this.yearVehicle;
+    }
+    getPictureVehicle()
+    {
+        return this.pictureVehicle;
+    }
+}
