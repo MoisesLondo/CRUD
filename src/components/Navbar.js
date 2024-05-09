8356c7e91e6a5cfb209b90c9e1db87bf2de0edcd
Luis <142854556+AstroLui@users.noreply.github.com>
2024-05-09 08:51:09 -0400

    feat: adding more the styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ class Navbar
     constructor()
     {
         const navbar = document.createElement("nav");
-        navbar.className = 'bg-color shadow-md rounded-sm';
+        navbar.className = 'bg-[#FFBFAF] shadow-md shadow-[#FF9E8F] rounded-sm';
         navbar.innerHTML = 
         `
             <div class="flex flex-col p-3">
-                <h1 class='text-3xl'>Lista de Vehiculos</h1>
-                <h1 class='text-sm -mt-2 text-color-txt'>Formulario</h1>
+                <h1 class='ml-5 text-4xl tracking-wider text-white'>Lista de Vehiculos</h1>
+                <h1 class='ml-6 text-md -mt-1 font-bold text-[#C63637]'>Formulario</h1>
             </div>
         `;
         document.body.appendChild(navbar).firstChild;
